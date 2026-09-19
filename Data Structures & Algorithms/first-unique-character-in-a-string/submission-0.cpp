cd67419a5bf353e72379773a771beec3a1fc8484
Shivanshu Sinha <141711821+shivdev79@users.noreply.github.com>
2026-09-19 20:24:24 +0530

    Add: first-unique-character-in-a-string - submission-0

---
+++ Data Structures & Algorithms/first-unique-character-in-a-string/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        map<char, int> mpp;
+        for(char ch : s)
+            mpp[ch]++;
+        for(int i = 0; i < s.size(); i++) {
+            if(mpp[s[i]] == 1)
+                return i;
+        }
+        return -1;
+    }
+};
