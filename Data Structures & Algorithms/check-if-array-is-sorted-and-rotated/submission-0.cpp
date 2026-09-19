77f089f32352332d9a06b54e955e2650c29981bf
Shivanshu Sinha <141711821+shivdev79@users.noreply.github.com>
2026-09-19 20:18:21 +0530

    Add: check-if-array-is-sorted-and-rotated - submission-0

---
+++ Data Structures & Algorithms/check-if-array-is-sorted-and-rotated/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int count = 0;
+        int n = nums.size();
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i] > nums[(i+1) % n]){
+                count = count + 1;
+
+            }
+        }
+        return count <= 1;
+    }
+};
